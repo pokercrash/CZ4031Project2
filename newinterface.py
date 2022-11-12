bda8104155738e22cee1ecd01e6063ad7cf59b42
charlotteteo <81745684+charlotteteo@users.noreply.github.com>
2022-11-12 12:12:17 +0800

    Add files via upload
    changed

--- newinterface.py
+++ newinterface.py
@@ -212,78 +212,31 @@ class QueryFrame(tk.Frame):
             self.text.tag_remove(node_type, '1.0', 'end')
 
 #GUI for displaying the analysis from the tree to the analysis frame
-class AnalysisFrame(tk.Frame):
-    def __init__(self, root):
-        tk.Frame.__init__(self, root)
-        self.text_font = font.Font(family='Fira Code Retina', size=12)
-        self.text = tk.Text(self, height=10, font=self.text_font)
-        
-        self.text.grid(row=1, column=0)
-        self.scrollbar = tk.Scrollbar(self, orient='vertical', command=self.text.yview)
-        self.scrollbar.grid(row=1, column=1, sticky='ns')
-        self.text.configure(yscrollcommand=self.scrollbar.set)
+def show_node_info(node):
+    raw_json = node.raw_json
+    idk = ['HASHAGGREGATE',"SORT","NESTED LOOP", "MERGE JOIN", "HASH JOIN", 'SEQ SCAN', 'INDEX SCAN', 'INDEX ONLY SCAN', 'BITMAP INDEX SCAN', 'CTE SCAN']
+    if node.id in idk:
+        return node.aqp_cost
+    else:
+        for index, (key, value) in enumerate(raw_json.items()):
+            if key == "Node Type":
+                for operations in ATTRIBUTE:
+                    if operations == value.upper():
+                        operation_type = ATTRIBUTE[operations]
+            if key in operation_type:
+                B = str(key) + ": " + str(value) + '\n'
+                return B       
+  
 
-        for node_type, color in NODE_COLORS.items():
-            self.text.tag_configure(node_type, background=color[0], foreground=color[1])
-        self.text.tag_configure('OTHER', background='#ff9800', foreground='black')
-
-        self.index_map = {}
-        self.query = None      
-
-    def show_node_info(self, node):
-        self.node = node
-        raw_json = node.raw_json
-        print(raw_json)
-        self.text.delete('1.0', 'end')
-        idk = ['HASHAGGREGATE',"SORT","NESTED LOOP", "MERGE JOIN", "HASH JOIN", 'SEQ SCAN', 'INDEX SCAN', 'INDEX ONLY SCAN', 'BITMAP INDEX SCAN', 'CTE SCAN']
-        if node.id in idk:
-            self.text.insert('end', node.aqp_cost)
-        else:
-            for index, (key, value) in enumerate(raw_json.items()):
-                if key == "Node Type":
-                    for operations in ATTRIBUTE:
-                        if operations == value.upper():
-                            operation_type = ATTRIBUTE[operations]
-                if key in operation_type:
-                    B = str(key) + ": " + str(value) + '\n'
-                    self.text.insert('end', B)       
-
-        # for index, (key, value) in enumerate(raw_json.items()):
-        #         if key == "Node Type":
-        #             for operations in ATTRIBUTE:
-        #                 if operations == value.upper():
-        #                     operation_type = ATTRIBUTE[operations]
-        #         if key in operation_type:
-        #             B = str(key) + ": " + str(value) + '\n'
-        #             self.text.insert('end', B)
-#executing the input query, linking to analysis page
-def execute_query(root_widget, query):
+def execute_query(query):
     plan = get_json(query)
     plan = plan[2:-2]
     
     plan = json.loads(plan)
-
-    top_level = tk.Toplevel(root_widget)
-    
-    top_level.title('Visualization')
-    top_level.iconphoto(False, tk.PhotoImage(file='tree.png'))
     query = query.replace('\n', ' ')
     query = sqlparse.format(query, reindent=True, keyword_case='upper')
-    
-    query_frame = QueryFrame(top_level)
-    query_frame.set_query(query)
-
-    query_frame.grid(row=0, column=0, sticky='ew')
-
-    analysis_frame = AnalysisFrame(top_level)
-    analysis_frame.grid(row=1, column=0, sticky='ew')
-
-    tree_frame =  TreeFrame(top_level)
-    tree_frame.grid(row=0, column=1, rowspan=2, sticky = 'w')
-
     root_node = annotation.build_tree([plan[0]['Plan']])[0]
     match_dict = annotation.build_invert_relation(query, root_node)
-
     print("jump")
     aggregate = ['HASHAGGREGATE','HASHAGGREGATE']
     sorts = ['SORT', 'SORT']
@@ -291,7 +244,7 @@ def execute_query(root_widget, query):
     scans = ['SEQ SCAN', 'INDEX SCAN', 'INDEX ONLY SCAN', 'BITMAP INDEX SCAN', 'CTE SCAN']
     dict2convert = {"NESTED LOOP":"nestloop", "MERGE JOIN":"mergejoin", "HASH JOIN": "hashjoin", 'SEQ SCAN':'seqscan', 'INDEX SCAN':'indexscan', 'INDEX ONLY SCAN':'indexonlyscan', 'BITMAP INDEX SCAN':'bitmapscan', 'CTE SCAN':'seqscan', 'HASHAGGREGATE':'hashagg', 'CTE SCAN':'tidscan', 'SORT':'sort'}
     cache = {}
-
+    node_aqp = []
     qep_cost = root_node.raw_json['Total Cost']
     for node in PreOrderIter(root_node):
         if node.id in loops:
@@ -333,23 +286,30 @@ def execute_query(root_widget, query):
             
         if node.aqp_cost == "":
             node.aqp_cost = "No other alternatives are possible."
+        else:
+            if node in match_dict:
+                for start, end in match_dict[node]:
+                    print(start, end, node.node_type, query[start:end])
+                node_aqp += [[node.aqp_cost,end,query[start:end]]]
+    return node_aqp
 
-    def on_click_listener(node):
-        analysis_frame.show_node_info(node)
 
-    def on_hover_listener(node):
-        if node in match_dict:
-            for start, end in match_dict[node]:
-                query_frame.highlight_text(start, end, node.node_type)
 
-    def on_hover_end_listener(node):
-        query_frame.clear_highlighting()
+def get_all_node_labels(query):
+    plan = get_json(query)
+    plan = plan[2:-2]
+    
+    plan = json.loads(plan)
+    query = query.replace('\n', ' ')
+    query = sqlparse.format(query, reindent=True, keyword_case='upper')
+    root_node = annotation.build_tree([plan[0]['Plan']])[0]
+    for node in PreOrderIter(root_node):
+        try:
+            print(show_node_info(node))
+        except:
+            print(node.id)
 
-    tree_frame.set_on_click_listener(on_click_listener)
-    tree_frame.set_on_hover_listener(on_hover_listener)
-    tree_frame.set_on_hover_end_listener(on_hover_end_listener)
 
-    tree_frame.draw_tree(root_node)
 
 def aqp_cost(query , permute):
     plan = get_json(query, permute)
--- project.py
+++ project.py
@@ -9,34 +9,40 @@ import node_types
 import psycopg2
 import annotation
 
+
 def retrieveInput():
-    inputValue=query_text.get('1.0', 'end-1c')
+    inputValue = query_text.get('1.0', 'end-1c')
     return inputValue
 
+
 if __name__ == '__main__':
-    
+
     root = tk.Tk()
     root.title('Input Query')
     root.iconphoto(False, tk.PhotoImage(file='tree.png'))
 
-    button_font = font.Font(family='Google Sans Display', size=12, weight='bold')
+    button_font = font.Font(
+        family='Google Sans Display', size=12, weight='bold')
     text_font = font.Font(family='Fira Code Retina', size=12)
     label_font = font.Font(family='Google Sans Display', size=12)
 
-    query_label = tk.Label(root, text='Enter your SQL query here', font=label_font)
+    query_label = tk.Label(
+        root, text='Enter your SQL query here', font=label_font)
     query_text = tk.Text(root, font=text_font, height=20)
 
-    #drawing the execute button
+    # drawing the execute button
     execute_button = tk.Button(root, text='EXECUTE', padx=12, bg='#7d8ed1', fg='white', font=button_font,
-                                 anchor='center', command=lambda: interface.get_json(retrieveInput()))
+                               anchor='center', command=lambda: interface.get_json(retrieveInput()))
 
-    #button execution to execute function
-    execute_button.bind('<Button-1>', lambda event: interface.execute_query(root, retrieveInput()))
+    # button execution to execute function
+    execute_button.bind(
+        '<Button-1>', lambda event: interface.execute_query(root, retrieveInput()))
 
-    query_scrollbar = tk.Scrollbar(root, orient='vertical', command=query_text.yview)
- 
+    query_scrollbar = tk.Scrollbar(
+        root, orient='vertical', command=query_text.yview)
+
     query_text.configure(yscrollcommand=query_scrollbar.set)
-  
+
     query_label.grid(row=0, sticky='w', padx=12, pady=(12, 0))
     query_text.grid(row=1, padx=(12, 0))
     query_scrollbar.grid(row=1, column=1, sticky='ns', padx=(0, 12))
